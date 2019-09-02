c7a1793df816bb13ec18d321ec3910d9d625a38b
yuval feldman <feldmanyuval@gmail.com>
2019-09-03 00:08:07 +0300

    wip foreach functionality

--- Agent/Commands/SystemCommands/AaSystemCommands.py
+++ Agent/Commands/SystemCommands/AaSystemCommands.py
@@ -2,6 +2,7 @@
 import time
 from AaSystem.LogAndPrint.Log import PrintRedAndLog, PrintAndLog
 from Agent.CommandInterface.ICommand import ICommand
+from Agent.Commands.ProcessControl.ProcessCommands import RunProcessPool
 
 TimeConversionsFromSeconds = {
     "-s": 1,
@@ -74,39 +75,63 @@ class Loop(ICommand):
                     "by parentheses that will be repeated in the loop\n")
 
 
-class ForEach(ICommand):
-    def Execute(self):
-        if self.CheckHelpRequested():
-            return
-        if self.CheckMinimunRequiredParameters(2):
-            return
-        readSource = self.request[0]
-        command = self.request[1]
-        swapString = self.request[2]
+def ReplaceForEach(command, oldString, newStrings):
+    return [command.replace(oldString, newString) for newString in newStrings]
 
-        if readSource == "-p":
-            parameters = self.request[3:]
-        elif readSource == "-f":
-            url = self.request[3]
-            try:
-                with open(url, "r") as file:
-                    commandLines = list(file.read().splitlines())
-                    PrintAndLog(f"Adding {len(commandLines)} commands using commands from {url}")
-                file.close()
-            except Exception as ex:
-                print(f"Failed to load command batch file, exception encountered:\n")
-                PrintRedAndLog(ex)
-        else:
-            PrintRedAndLog(f"No supported read source parameter {readSource}")
-            return
+
+def GetParamArrayFromFile(path):
+    try:
+        with open(path, "r") as file:
+            parameters = list(file.read().splitlines())
+            PrintAndLog(f"Adding {len(parameters)} commands using commands from {path}")
+        file.close()
         splitParameters = []
-
         for param in parameters:
             if "," in param:
                 splitParameters.extend(param.split(","))
                 continue
             splitParameters.append(param)
-        newCommands = [command.replace(swapString, param) for param in splitParameters]
+        return splitParameters
+    except Exception as ex:
+        print(f"Failed to load command batch file, exception encountered:\n")
+        PrintRedAndLog(ex)
+
+
+def GetParamArrayFromString(string):
+    if not string:
+        return ""
+    return string.split(",")
+
+
+def GetCommandsFromSwapDictionary(command, swapDictionary):
+    return []
+
+
+def GetNewCommands(readSource, command, swaps):
+    swapDictionary = {}
+
+    for pair in swaps:
+        kvp = str(pair).split("=")
+        if len(kvp) != 2:
+            PrintRedAndLog("Invalid syntax")
+        if readSource == "-p":
+            swapDictionary[kvp[0]] = GetParamArrayFromString(kvp[1])
+        elif readSource == "-f":
+            swapDictionary[kvp[0]] = GetParamArrayFromFile(kvp[1])
+        else:
+            PrintRedAndLog(f"No supported read source parameter {readSource}")
+            return
+
+    return GetCommandsFromSwapDictionary(command, swapDictionary)
+
+
+class ForEach(ICommand):
+    def Execute(self):
+        if self.CheckHelpRequested():
+            return
+        if self.CheckMinimunRequiredParameters(3):
+            return
+        newCommands = GetNewCommands(self.request[0], self.request[1], self.request[2:])
         self.context.CommandQueue.EnqueueCommandsNext(newCommands)
 
     def HelpRequested(self):
@@ -118,40 +143,16 @@ class ForEach(ICommand):
                     "                     'function' - an Attack Agent function surrounded "
                     "by parentheses that will be repeated in the loop\n")
 
+
 class AsyncForEach(ICommand):
     def Execute(self):
         if self.CheckHelpRequested():
             return
-        if self.CheckMinimunRequiredParameters(2):
+        if self.CheckMinimunRequiredParameters(4):
             return
-        readSource = self.request[0]
-        command = self.request[1]
-        swapString = self.request[2]
-
-        if readSource == "-p":
-            parameters = self.request[3:]
-        elif readSource == "-f":
-            url = self.request[3]
-            try:
-                with open(url, "r") as file:
-                    commandLines = list(file.read().splitlines())
-                    PrintAndLog(f"Adding {len(commandLines)} commands using commands from {url}")
-                file.close()
-            except Exception as ex:
-                print(f"Failed to load command batch file, exception encountered:\n")
-                PrintRedAndLog(ex)
-        else:
-            PrintRedAndLog(f"No supported read source parameter {readSource}")
-            return
-        splitParameters = []
-
-        for param in parameters:
-            if "," in param:
-                splitParameters.extend(param.split(","))
-                continue
-            splitParameters.append(param)
-        newCommands = [command.replace(swapString, param) for param in splitParameters]
-        self.context.CommandQueue.EnqueueCommandsNext("asyncpool")
+        newCommandsString = ' '.join(GetNewCommands(self.request[1], self.request[2], self.request[3:]))
+        processPoolCommand = f"asyncpool {self.request[0]} {newCommandsString}"
+        self.context.CommandQueue.EnqueueCommandsNext(processPoolCommand)
 
     def HelpRequested(self):
         PrintAndLog("\n"
